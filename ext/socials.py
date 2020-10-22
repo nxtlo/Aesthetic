266f570d7d0410510b895f7232295c51f2764d2a
fate <dhmony-99@hotmail.com>
2020-10-22 03:49:02 +0300

    testing stuff with cpp :alien:

---
+++ ext/socials.py
@@ -0,0 +1,10 @@
+import discord
+
+twitter = "[Click Here](https://twitter.com/helfate)"
+insta = "[Click Here](https://instagram.com/nxtlo)"
+youtube = "[Click Here](https://youtube.com/channel/UC4acY39W-lBjgiqOBsaoqJw)"
+bungie = "[Click Here](https://www.bungie.net/en/Profile/254/20315338/Fate)"
+github = "[Click Here](https://github.com/nxtlo)"
+steam = "[Click Here](https://steamcommunity.com/id/LordhaveFate/)"
+
+emj = discord.utils.get
