47e109ea37b062598632862096c316e8f4309c8d
fate <dhmony-99@hotmail.com>
2020-10-24 00:44:21 +0300

    socials.py -> utils.py

--- core/cogs/commands.py
+++ core/cogs/commands.py
@@ -1,8 +1,8 @@
 import discord
+
 from discord.ext import commands
-import sys
-import asyncio
-from ext import socials as sc
+from discord import utils as us
+from ext import utils as u
 
 class Commands(commands.Cog):
     def __init__(self, bot):
@@ -11,26 +11,25 @@ class Commands(commands.Cog):
 
     @commands.command(name='owner', aliases=['socials'] ,discription="Fate's social media stuff")
     async def owner_command(self, ctx):
+        
+        insta = us.get(self.bot.emojis, name="insta")
+        twitter = us.get(self.bot.emojis, name="twitter")
+        bungie = us.get(self.bot.emojis, name="bungie")
+        steam = us.get(self.bot.emojis, name="steam")
+        github = us.get(self.bot.emojis, name="github")
+        utube = us.get(self.bot.emojis, name="youtube")
 
         embed = discord.Embed(
             title="*Fate's social info*",
             color = ctx.author.color,
             timestamp =ctx.message.created_at,
         )
-
-        insta = sc.emj(self.bot.emojis, name='insta')
-        twitter = sc.emj(self.bot.emojis, name='twitter')
-        utube = sc.emj(self.bot.emojis, name='youtube')
-        steam = sc.emj(self.bot.emojis, name='steam')
-        github = sc.emj(self.bot.emojis, name='github')
-        bungie = sc.emj(self.bot.emojis, name="bungie")
-
-        embed.add_field(name=f"{insta} ```Instagram```", value=sc.insta, inline=True)
-        embed.add_field(name=f"{twitter} ```Twitter```", value=sc.twitter, inline=True)
-        embed.add_field(name=f"{utube} ```Youtube```", value=sc.youtube, inline=True)
-        embed.add_field(name=f"{steam} ```Steam```", value=sc.steam, inline=True)
-        embed.add_field(name=f"{github} ```Github```", value=sc.github, inline=True)
-        embed.add_field(name=f"{bungie} ```Bungie```", value=sc.bungie)
+        embed.add_field(name=f"{insta} ```Instagram```", value=u.url.insta, inline=True)
+        embed.add_field(name=f"{twitter} ```Twitter```", value=u.url.twitter, inline=True)
+        embed.add_field(name=f"{utube} ```Youtube```", value=u.url.youtube, inline=True)
+        embed.add_field(name=f"{steam} ```Steam```", value=u.url.steam, inline=True)
+        embed.add_field(name=f"{github} ```Github```", value=u.url.github, inline=True)
+        embed.add_field(name=f"{bungie} ```Bungie```", value=u.url.bungie)
         embed.set_author(name=f"{self.bot.user.name}", icon_url=self.bot.user.avatar_url)
         embed.set_footer(text=f"Requested {ctx.author}")
         await ctx.send(embed=embed)
---
+++ ext/utils.py
@@ -0,0 +1,26 @@
+import discord
+
+from discord.ext.commands import bot
+from discord import utils as u
+
+
+class url:
+    """
+    a Class for the "Click here" values in Embed
+    """
+    insta = "[Click Here](https://instagram.com/nxtlo)"
+    twitter = "[Click Here](https://twitter.com/helfate)"
+    bungie = "[Click Here](https://www.bungie.net/en/Profile/254/20315338/Fate)"
+    github = "[Click Here](https://github.com/nxtlo)"
+    steam = "[Click Here](https://steamcommunity.com/id/LordhaveFate/)"
+    youtube = "[Click Here](https://youtube.com/channel/UC4acY39W-lBjgiqOBsaoqJw)"
+"""
+class emj:
+
+    insta = u.get(bot.emojis, name="insta")
+    twitter = u.get(bot.emojis, name="twitter")
+    bungie = u.get(bot.emojis, name="bungie")
+    steam = u.get(bot.emojis, name="steam")
+    github = u.get(bot.emojis, name="github")
+    utube = u.get(bot.emojis, name="youtube")
+    """
