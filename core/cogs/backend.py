fd3059f36ee5523ef7818181dbcedd7d48d75c2c
nxtlo <dhmony-99@hotmail.com>
2020-12-23 10:18:20 +0300

    added db commands to format tables/schema

--- core/cogs/backend.py
+++ core/cogs/backend.py
@@ -1,15 +1,20 @@
 """
-The backend for the bot
+The backend and database for the bot
 """
 from data import db
 from discord.ext.commands import command, is_owner, Cog, has_permissions, guild_only, bot_has_permissions, group
 from discord.ext import commands
-from discord import Embed
+from discord import Embed, TextChannel
 from datetime import datetime
 from time import strftime
 from core.ext.utils import color
+from typing import Optional
+from ..ext import check
+#--------------
+import requests
+import mystbin
+import subprocess
 import asyncio
-import typing
 import logging
 import sqlite3
 import discord
@@ -17,9 +22,10 @@ import discord
 class Sql(Cog):
     def __init__(self, bot):
         self.bot = bot
-        self._log_channel = 512946130691162112
+        self._log_channel = 789614938247266305
         self.default_prefix = "a."
         self._logger = logging.getLogger(__name__)
+        self._mystbin = mystbin.Client(session=requests.Session())
     
     def db_update(self):
         db.multiexec("INSERT OR IGNORE INTO Guilds (id) VALUES (?)", ((guild.id,) for guild in self.guild))
@@ -29,17 +35,6 @@ class Sql(Cog):
     async def db(self, ctx):
         """Commands for returning stuff from the database"""
         pass
-
-    @db.command(name="help")
-    async def db_help(self, ctx):
-        e = Embed(
-            title="Database commands.",
-            description="`init`: initializes the bot to the database\n "
-                        "`info`: gives info about the database system\n "
-                        "`SELECT`: returns anything from the database",
-            color=color.invis(self)
-        )
-        await ctx.send(embed=e)
 
 
     @db.command(name="init", hidden=True)
@@ -64,8 +59,8 @@ class Sql(Cog):
                 await ctx.send(":thumbsup:")
                 asyncio.sleep(1)
                 await ctx.message.delete()
-        except Exception:
-            raise self._logger
+        except Exception as e:
+            await ctx.send(f"```{e.__class__.__name__}: {e}```")
     
     @db.command("SELECT", aliases=['select'])
     @is_owner()
@@ -76,16 +71,13 @@ class Sql(Cog):
         Example: db select prefix Guilds id
         
         This will basically return the `prefix` from `Guilds` table\nwhere the guild `id` is `ctx.guild.id` or `member.guild.id`
-
         Another Example: db SELECT * bans id
-
         This one will select everything from bans where id = member.guild.id
-
         You can get data for other guilds by doing this:\n\ndb select * Guilds id <guild_id>
         """
         
         def _all():
-            all_guilds = inp or ctx.guild.id
+            all_guilds = inp or ctx.guild.id or ctx.message.guild.id
             snowflake = db.cur.execute(f"SELECT {option} FROM {from_table} WHERE {coloumn} = ?", (all_guilds,))
 
             for table in snowflake.fetchall():
@@ -94,11 +86,70 @@ class Sql(Cog):
             e = Embed(color=color.invis(self))
             e.add_field(name="Table name:" ,value=f'```{from_table}```', inline=False)
             e.add_field(name="Resaults:", value=_all(), inline=False)
-            await ctx.send(embed=e)
         except Exception as e:
-            await ctx.send(e)
-        
+            await ctx.send(f"```{e.__class__.__name__}: {e}```")
     
+    @db.command(name="pragma", aliases=['pr'])
+    @is_owner()
+    async def _pragma(self, ctx, table: str, *, encoding=None):
+        """
+        Format the database and render it as rST format
+        
+        You use the `-> json` after the table name to convert the pragma to json
+
+        excample: db pragma/pr Guilds -> json
+        """
+        try:
+            async with ctx.typing():
+                # json format
+                if table is not None and encoding == '-> json':
+
+                    cmd = f'sqlite-utils {db.DB_PATH} "select * from {table}" | python -mjson.tool'
+                    schema = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True, close_fds=True, encoding='utf8')
+                    
+                    if len(cmd) < 2000:
+                        await ctx.send(f"```json\n{schema.communicate()[0]}\n```")
+                    
+                    elif len(cmd) > 2000:
+                        content = self._mystbin.post(f"{schema.communicate()[0]}", syntax='json').url
+                        return await ctx.send(f"Too many results... Uploaded to mystbin -> {content}")
+                
+                # if encodibg != json then send it as a normal rST format
+
+                else:
+                    cmd = f'sqlite-utils {db.DB_PATH} "select * from {table}" --table'
+                    if table:
+                        if len(cmd) < 2000:
+                            pragma = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True, close_fds=True, encoding='utf8')
+                            await ctx.send(f"```\n{pragma.communicate()[0]}\n```")
+                        else:
+                            if len(cmd) > 2000:
+                                content = self._mystbin.post(f"{pragma.communicate()[0]}", syntax='sql').url
+                                return await ctx.send(f"Too many results... Uploaded to mystbin -> {content}")
+        except Exception as e:
+            await ctx.send(f"```{e.__class__.__name__}: {e}```")
+        finally:
+            pass
+
+    @db.command(name="schema")
+    @is_owner()
+    async def _schema(self, ctx):
+        """Show the database schema"""
+        try:
+            async with ctx.typing():
+                cmd = f'sqlite-utils tables {db.DB_PATH} --schema --table'
+                schema = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True, close_fds=True, encoding='utf8')
+                
+                if len(cmd) < 2000:
+                    await ctx.send(f"```sql\n{schema.communicate()[0]}\n```")
+                
+                elif len(cmd) > 2000:
+                    content = self._mystbin.post(f"{schema.communicate()[0]}", syntax='sql').url
+                    return await ctx.send(f"Too many results... Uploaded to mystbin -> {content}")
+        
+        except Exception as e:
+            await ctx.send(f"```{e.__class__.__name__}: {e}```")
+            
     @db.command(name="info")
     async def db_info(self, ctx):
         e = Embed(
