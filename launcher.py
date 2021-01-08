44d61a19cebfb03d7f8f66f95906f804328edc8e
nxtlo <dhmony-99@hotmail.com>
2021-01-08 04:01:12 +0100

    uvloop

--- launcher.py
+++ launcher.py
@@ -1,7 +1,16 @@
 from core import Amaya
+import asyncio
+
+try:
+    import uvloop
+except ImportError:
+    pass
+else:
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 def main():
     bot = Amaya()
+    uvloop.install()
     bot.loop.run_until_complete(bot.pool_connect())
     bot.run()
 
